1bf16727d25f8237e1b21d64782a561853d6150c
Mishri <kiwixmishi@gmail.com>
2023-01-25 12:14:07 +0100

    WAITNG FOR FIX

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,30 @@ function getComputerChoice() {
     //round off random value multiplied by 3 then add one
     const value = Math.floor(Math.random() * 3) + 1;
     if (value === 1) {
-        return "Rock";
+        return "rock";
     }else if (value === 2) {
-        return "Paper";
+        return "paper";
     }else {
-        return "Scissors";
+        return "scissors";
     }
+}
+//get choice from pc
+let playerChoice = "rock"; 
+
+//return number for choice
+function choiceNumber(value) {
+    const valueCase = value.toLowerCase();
+    if (valueCase === "rock") {
+        return 1;
+    }else if (valueCase === "paper") {
+        return 2;
+    }else if (valueCase === "scissors") {
+        return 4;
+    }else {
+        return "provide a proper value";
+    } 
+}
+
+function playRound( playerSelection, randomChoice = getComputerChoice()) {
+    //convert sign to value
 }

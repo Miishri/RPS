449ba53fcd5e431a9ca6bb2b19aa7dd10345df8c
Mishri <kiwixmishi@gmail.com>
2023-01-25 02:26:16 +0100

    add random choice func()

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    //round off random value multiplied by 3 then add one
+    const value = Math.floor(Math.random() * 3) + 1;
+    if (value === 1) {
+        return "Rock";
+    }else if (value === 2) {
+        return "Paper";
+    }else {
+        return "Scissors";
+    }
+}
